c36f2ee091c384e21fed5c3ecd707594add95075
Michael Amadi <amadimichaeld@gmail.com>
2023-03-19 16:04:47 +0100

    Dev (#2)
    * readme and readme exercises

---
+++ test/Return1.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {HuffConfig} from "foundry-huff/HuffConfig.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+contract Return1Test is Test {
+    address public return1;
+
+    function setUp() public {
+        return1 = HuffDeployer.config().deploy("Return1");
+    }
+
+    function testReturn1() public {
+        (bool success, bytes memory ret) = return1.call("");
+        require(success, "call to return1 address failed");
+        assertEq(
+            abi.decode(ret, (uint256)),
+            1,
+            "Expected a call to return1 address to return 1"
+        );
+    }
+}
